3c294c17384ef75ff189a26f11f2a286fe888726
Bryan Astacio <brya0196@gmail.com>
2025-08-17 12:16:25 -0400

    refactor: remove get login method

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
     delete "/logout", to: "sessions#destroy"
   end
 
-  get "/login", to: "sessions#new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
